647f8861f729f0b23180c967c3cc595071967338
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-02-07 17:04:00 +0100

    Update player.cs

--- player.cs
+++ player.cs
@@ -6,8 +6,14 @@ public class player : MonoBehaviour
 {
     private bool jumpKeyWasPressed;
     private float horizontalInput;
+    private float verticalInput;
     private Rigidbody rigidbodyComponent;
-    
+    private bool isGrounded;
+    private int superJumpsRemaining;
+
+    [SerializeField] private Transform GroundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,23 +23,54 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-           jumpKeyWasPressed = true;
+            jumpKeyWasPressed = true;
         }
-        horizontalInput = Input.GetAxis("Horizontal");
 
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
     }
 
     // fixed update
     private void FixedUpdate()
     {
-        if(jumpKeyWasPressed)
-        {
-            rigidbodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
-            jumpKeyWasPressed = false;
-        }   
+        rigidbodyComponent.velocity = new Vector3(horizontalInput * 6, rigidbodyComponent.velocity.y, 0);
 
-        rigidbodyComponent.velocity = new Vector3(horizontalInput * 5, rigidbodyComponent.velocity.y, 0);    
+        if (Physics.OverlapSphere(GroundCheckTransform.position, 1, playerMask).Length == 0)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            float jumpPower = 5;
+
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+
+    if (other.gameObject.layer==9)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+
 }
